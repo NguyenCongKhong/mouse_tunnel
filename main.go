6c12597d262cde59baea9756c293de1499b54b4e
NguyenCongKhong <48519058+NguyenCongKhong@users.noreply.github.com>
2022-05-10 23:38:00 +0700

    fix path

--- main.go
+++ main.go
@@ -9,14 +9,14 @@ func main() {
     log.Printf("Start tunnel\n")
     var cmd *exec.Cmd
     var err error
-    cmd = exec.Command("chmod", "+x", "./chisel")
+    cmd = exec.Command("chmod", "+x", "../chisel")
     err = cmd.Run()
     if err != nil {
         log.Printf("run chmod return error %v\n", err)
         return
     }
-    cmd = exec.Command("./chisel", "--port", "8000", "--backend", "--socks5", "--reverse")
-    err = cmd.Start()
+    cmd = exec.Command("../chisel", "--port", "8000", "--backend", "--socks5", "--reverse")
+    err = cmd.Run()
     if err != nil {
         log.Printf("start chisel return error %v\n", err)
     }
