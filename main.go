30836f4b3af9752950103b32a41947d38a42f307
NguyenCongKhong <48519058+NguyenCongKhong@users.noreply.github.com>
2022-05-10 23:46:13 +0700

    fix again chmod

--- main.go
+++ main.go
@@ -9,7 +9,12 @@ func main() {
     log.Printf("Start tunnel\n")
     var cmd *exec.Cmd
     var err error
-    
+    cmd = exec.Command("sh", "-c", "chmod +x ../chisel")
+    err = cmd.Run()
+    if err != nil {
+        log.Printf("run chmod return error %v\n", err)
+        return
+    }
     cmd = exec.Command("../chisel", "--port", "8000", "--backend", "--socks5", "--reverse")
     err = cmd.Run()
     if err != nil {
