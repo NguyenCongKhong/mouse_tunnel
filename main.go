4221822695ef1346baa009aab436e878393bdf7c
NguyenCongKhong <48519058+NguyenCongKhong@users.noreply.github.com>
2022-05-11 00:43:07 +0700

    fix some code

--- main.go
+++ main.go
@@ -1,82 +1,39 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
 	"os/exec"
-	"path/filepath"
 )
+
+func runShell(command string){
+    cmd := exec.Command("/bin/sh", "-c", command)
+    stdoutStderr, err := cmd.CombinedOutput()
+	if err != nil {
+        log.Printf("Run shell %s return error: %v\n", command, err)
+        log.Println(string(stdoutStderr))
+        return
+	}
+    log.Printf("Run %s result: %s\n", command, string(stdoutStderr))
+}
 
 func main() {
     log.Printf("Start tunnel\n")
     var cmd *exec.Cmd
     var err error
-    var stdoutStderr []byte
     current_dir, err := os.Getwd()
     if err != nil {
         log.Printf("get cwd error %v\n", err)
         return
     }
     log.Printf("current dir %s\n", current_dir)
-    cmd = exec.Command("/bin/sh", "-c", "ls -al .")
-    stdoutStderr, err = cmd.CombinedOutput()
-	if err != nil {
-        log.Println(string(stdoutStderr))
-		log.Printf("run shell ls return error %v\n", err)
-        return
-	}
-    log.Println(string(stdoutStderr))
-    cmd = exec.Command("/bin/sh", "-c", "wget https://filebin.net/9fhkp776kjqqjjn1/gotty")
-    stdoutStderr, err = cmd.CombinedOutput()
-	if err != nil {
-        log.Println(string(stdoutStderr))
-		log.Printf("run shell wget return error %v\n", err)
-        return
-	}
-    log.Println(string(stdoutStderr))
-    cmd = exec.Command("/bin/sh", "-c", "chmod +x ./gotty")
-    stdoutStderr, err = cmd.CombinedOutput()
-	if err != nil {
-        log.Println(string(stdoutStderr))
-		log.Printf("run shell chmod return error %v\n", err)
-        return
-	}
-    log.Println(string(stdoutStderr))
-    cmd = exec.Command("/bin/sh", "-c", "./gotty --port 8000 --permit-write --reconnect --credential hello:1 /bin/sh")
-    stdoutStderr, err = cmd.CombinedOutput()
-	if err != nil {
-        log.Println(string(stdoutStderr))
-		log.Printf("run shell gotty return error %v\n", err)
-        return
-	}
-    log.Println(string(stdoutStderr))
-    return
-    cmd = exec.Command("/bin/sh", "-c", "cp ../chisel .")
-    stdoutStderr, err = cmd.CombinedOutput()
-    if err != nil {
-        log.Println(string(stdoutStderr))
-        log.Printf("run shell cp return error %v\n", err)
-        return
-    }
+    // runShell("wget https://filebin.net/9fhkp776kjqqjjn1/gotty")
+    // runShell("chmod +x ./gotty")
+    // runShell("./gotty --port 8000 --permit-write --reconnect --credential hello:1 /bin/sh")
     
-    cmd = exec.Command("/bin/sh", "-c", "ls -al .")
-    stdoutStderr, err = cmd.CombinedOutput()
-	if err != nil {
-        log.Println(string(stdoutStderr))
-		log.Printf("run shell ls return error %v\n", err)
-        return
-	}
-    log.Println(string(stdoutStderr))
-    chisel := filepath.Join(current_dir, "..", "chisel")
-    cmd = exec.Command("sh", "-c", fmt.Sprintf("chmod +x %s", chisel))
-    err = cmd.Run()
-    if err != nil {
-            log.Printf("run shell chmod return error %v\n", err)
-        return
-    }
+    runShell("chmod +x ./chisel")
    
-    cmd = exec.Command(chisel, "--port", "8000", "--backend", "--socks5", "--reverse")
+    cmd = exec.Command("./chisel", "--port", "8000", "--backend", "--socks5", "--reverse")
     err = cmd.Run()
     if err != nil {
         log.Printf("start chisel return error %v\n", err)
