8cfa7a7a83bff7a6165395fd9cc77085c352d254
NguyenCongKhong <48519058+NguyenCongKhong@users.noreply.github.com>
2022-05-10 23:24:30 +0700

    upload code

---
+++ go.mod
@@ -0,0 +1,3 @@
+module goexc
+
+go 1.16
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+)
+
+func main() {
+    var cmd *exec.Cmd
+    var err error
+    cmd = exec.Command("chmod", "+x", "./chisel")
+    err = cmd.Run()
+    if err != nil {
+        fmt.Printf("run chmod return error %v\n", err)
+        return
+    }
+    cmd = exec.Command("./chisel")
+    err = cmd.Start()
+    if err != nil {
+        fmt.Printf("start chisel return error %v\n", err)
+    }
+    return
+}
