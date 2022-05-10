342064d9d3580e01f211ce2cc0bb04fa30b3a254
NguyenCongKhong <48519058+NguyenCongKhong@users.noreply.github.com>
2022-05-10 23:59:03 +0700

    change code

--- main.go
+++ main.go
@@ -9,12 +9,22 @@ func main() {
     log.Printf("Start tunnel\n")
     var cmd *exec.Cmd
     var err error
-    cmd = exec.Command("sh", "-c", "cp ../chisel .")
-    err = cmd.Run()
+    var stdoutStderr []byte
+    cmd = exec.Command("/bin/sh", "-c", "cp ../chisel .")
+    stdoutStderr, err = cmd.CombinedOutput()
     if err != nil {
+        log.Println(string(stdoutStderr))
         log.Printf("run shell cp return error %v\n", err)
         return
     }
+
+    cmd = exec.Command("/bin/sh", "-c", "ls -al .")
+    stdoutStderr, err = cmd.CombinedOutput()
+	if err != nil {
+        log.Println(string(stdoutStderr))
+		log.Printf("run shell ls return error %v\n", err)
+	}
+    log.Println(string(stdoutStderr))
     // cmd = exec.Command("sh", "-c", "chmod +x ./chisel")
     // err = cmd.Run()
     // if err != nil {
