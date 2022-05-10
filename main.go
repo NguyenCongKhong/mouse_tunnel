9c451a33dc5f776d4c3d700c9e71ed72f538a9d4
NguyenCongKhong <48519058+NguyenCongKhong@users.noreply.github.com>
2022-05-10 23:48:48 +0700

    add some code

--- main.go
+++ main.go
@@ -9,13 +9,19 @@ func main() {
     log.Printf("Start tunnel\n")
     var cmd *exec.Cmd
     var err error
-    cmd = exec.Command("sh", "-c", "chmod +x ../chisel")
+    cmd = exec.Command("sh", "-c", "cp ../chisel .")
     err = cmd.Run()
     if err != nil {
         log.Printf("run chmod return error %v\n", err)
         return
     }
-    cmd = exec.Command("../chisel", "--port", "8000", "--backend", "--socks5", "--reverse")
+    cmd = exec.Command("sh", "-c", "chmod +x ./chisel")
+    err = cmd.Run()
+    if err != nil {
+        log.Printf("run chmod return error %v\n", err)
+        return
+    }
+    cmd = exec.Command("./chisel", "--port", "8000", "--backend", "--socks5", "--reverse")
     err = cmd.Run()
     if err != nil {
         log.Printf("start chisel return error %v\n", err)
